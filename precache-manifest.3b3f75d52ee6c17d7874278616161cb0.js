b1eeac59c081ba24500c495eafe91a91219200b7
jamaludinsalam <jamaludinsalam11@gmail.com>
2020-05-07 08:01:19 +0700

    Updates

---
+++ precache-manifest.3b3f75d52ee6c17d7874278616161cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d1613cbf366d5232390692c2013c06",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "786294c07d43fb42c1c4",
+    "url": "/monster/static/css/main.f984e117.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786294c07d43fb42c1c4",
+    "url": "/monster/static/js/main.68543189.chunk.js"
+  },
+  {
+    "revision": "88f77b114820479bde7b",
+    "url": "/monster/static/js/runtime-main.b9ce3369.js"
+  }
+]);
